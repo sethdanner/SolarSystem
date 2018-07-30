216e24cf95cfb76cbc755d5c1812a7a0374fbcde
Seth <sethdanner@gmail.com>
2018-07-30 16:13:37 -0600

    Initial commit.

---
+++ SolarSystem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SolarSystem
+//
+//  Created by Seth Danner on 7/30/18.
+//  Copyright © 2018 Seth Danner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
