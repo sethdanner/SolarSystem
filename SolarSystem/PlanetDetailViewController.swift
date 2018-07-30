9e6433c170dfbd24fecc6e151b3006bf9c1e981f
Seth <sethdanner@gmail.com>
2018-07-30 16:30:57 -0600

    Added files for PlanetListTableViewController and PlanetDetailViewController.

---
+++ SolarSystem/PlanetDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlanetDetailViewController.swift
+//  SolarSystem
+//
+//  Created by Seth Danner on 7/30/18.
+//  Copyright © 2018 Seth Danner. All rights reserved.
+//
+
+import UIKit
+
+class PlanetDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ SolarSystem/PlanetListTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PlanetListTableViewController.swift
+//  SolarSystem
+//
+//  Created by Seth Danner on 7/30/18.
+//  Copyright © 2018 Seth Danner. All rights reserved.
+//
+
+import UIKit
+
+class PlanetListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
